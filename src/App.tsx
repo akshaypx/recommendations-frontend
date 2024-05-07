ed90e8bce695c633c218a64767f5fce48bece286
NAGARRO\akshaysrivastava <akshaysrivastava46@gmail.com>
2024-05-07 17:09:24 +0530

    fix: linting errors fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,36 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect } from "react";
 import {
   getItemsThatSimilarUsersBought,
   getItemsThatSimilarUsersViewed,
   getItemsThatSimilarUsersBoughtLocation,
-} from './api/api';
-import './App.css';
-import Section from './components/Section';
-import { LocationContext } from './api/context';
-import CustomSection from './components/CategorySection';
+} from "./api/api";
+import Section from "./components/Section";
+import { LocationContext } from "./api/context";
+import CustomSection from "./components/CategorySection";
 
 function App() {
   const location = useContext(LocationContext);
   useEffect(() => {
-    console.log(location, 'rendered again');
+    console.log(location, "rendered again");
   }, [location]);
 
   return (
     <div className="flex flex-col gap-4 p-2 pl-1">
       <CustomSection />
-      <Section title={'Similar Users also Bought'} fetchData={getItemsThatSimilarUsersBought} />
-      <Section title={'Similar Users also Viewed'} fetchData={getItemsThatSimilarUsersViewed} />
+      <Section
+        title={"Similar Users also Bought"}
+        fetchData={getItemsThatSimilarUsersBought}
+      />
+      <Section
+        title={"Similar Users also Viewed"}
+        fetchData={getItemsThatSimilarUsersViewed}
+      />
       {location?.value && (
         <Section
-          title={'Products Bought in your Region'}
-          fetchData={() => getItemsThatSimilarUsersBoughtLocation(location?.value)}
+          title={"Products Bought in your Region"}
+          fetchData={() =>
+            getItemsThatSimilarUsersBoughtLocation(location?.value)
+          }
         />
       )}
     </div>
